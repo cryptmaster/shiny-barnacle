matrix P and Pdat array may be correct probability calculation necessary

a_run_edge_advProb_eval.py
@@ -45,9 +45,12 @@ for uid in data['Train Reviewer List'] :
 	# For each star rating, save index value of associated business_idx and reviewer_idx
         r[stars].append(business_idx[data['Review Information'][rid]['business_id']]);
         c[stars].append(reviewer_idx[uid]);
+
+I = sp.csr_matrix(([],([],[])),shape=[B,R]);
 for s in r.keys() :
     # map the reviewer's reviews to businesses
     A[s] = sp.csr_matrix((np.ones((len(r[s]),)),(r[s],c[s])),shape=[B,R]);
+    I += A[s];
 print '   %.2f seconds elapsed'%(time.clock()-start);
 
 print 'Building graph...'
@@ -76,32 +79,37 @@ print '\t%.2f seconds elapsed'%(time.clock()-start);
 print 'Building Numerator && Denominator...'
 #numerator = sp.csr_matrix(([],([],[])),shape=[R,R]);
 #denominator = sp.csr_matrix(([],([],[])),shape=[R,R]);
-numerator = {};
-demoinator = {};
-#denominator = Ap.sum(axis=1)
+numerator = {}
+denominator = {}
 for s in pos_list :
-    print "For S = %d"%(s);
-    numerator[s] = A[s].dot(Ap.T);
-    denominator[s] = A[s].dot(A[s].T);
-print '\t%.2f seconds elapsed'%(time.clock()-start);
+    print "For S = %d"%(s)
+    numerator[s] = A[s].dot(Ap.T)
+    denominator[s] = A[s].dot(I.T)
+print '\t%.2f seconds elapsed'%(time.clock()-start)
 
 print "You have the numerator now with: "
-print "\tshape: " + str(numerator.shape);
-print "\tstored elements: " + str(numerator.getnnz());
+print "\tshape: " + str(numerator[5].shape)
+print "\tstored elements: " + str(numerator[5].getnnz())
 print "You have the denominator now with: "
-print "\tshape: " + str(denominator.shape);
-print "\tstored elements: " + str(denominator.getnnz());
-print '\n\t%.2f seconds elapsed'%(time.clock()-start);
+print "\tshape: " + str(denominator[5].shape)
+print "\tstored elements: " + str(denominator[5].getnnz())
+print '\n\t%.2f seconds elapsed'%(time.clock()-start)
 
-#numcol = sp.coo_matrix(numerator)
-#demcol = sp.coo_matrix(denominator)
+print 'Creating normalization...'
+Ncoo = sp.coo_matrix(numerator[5])
+Dcoo = sp.coo_matrix(denominator[5])
 
-#halfeq = sp.linalg.inv(denom)
-#lu = sp.linalg.splu(denom)
-#eye = np.eye(ksize)
-#halfeq = lu.solve(eye)
-#k = numerator.dot(halfeq);
-#k = sp.linalg.spsolve(denom, numerator)
+offset = np.ones((len(Dcoo.data),))
+Noff = sp.csr_matrix((offset, (Dcoo.row, Dcoo.col)),shape=[B,B])
+Doff = sp.csr_matrix(((offset.dot(2)), (Dcoo.row, Dcoo.col)),shape=[B,B])
+print '\t%.2f seconds elapsed'%(time.clock()-start)
+
+print 'Calculating advanced probability...'
+Numerator = numerator[5] + Noff
+Denominator = denominator[5] + Doff
+Pdat = Numerator.tocoo().data / Denominator.tocoo().data
+P = sp.csr_matrix((Pdat, (Dcoo.row, Dcoo.col)),shape=[B,B])
+print '\t%.2f seconds elapsed'%(time.clock()-start)
 
 
 #############################