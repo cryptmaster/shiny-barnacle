1773a527aa588582b1bb39aa4d6a1fd94ee6156d
apfannenstein <alpfannenstein@gmail.com>
2016-12-02 16:06:50 -0500

    minor mods to try debugging this testcase for it to work

--- tfidf/tfidf_test.py
+++ tfidf/tfidf_test.py
@@ -17,8 +17,7 @@ def get_expected_idf(num_docs_total, num_docs_term):
 class TfIdfTest(unittest.TestCase):
   def testGetIdf(self):
     """Test querying the IDF for existent and nonexistent terms."""
-    my_tfidf = tfidf.TfIdf("tfidf_testcorpus.txt", \
-                           DEFAULT_IDF = DEFAULT_IDF_UNITTEST)
+    my_tfidf = tfidf.TfIdf("tfidf_testcorpus.txt", DEFAULT_IDF = DEFAULT_IDF_UNITTEST)
 
     # Test querying for a nonexistent term.
     self.assertEqual(DEFAULT_IDF_UNITTEST, my_tfidf.get_idf("nonexistent"))
@@ -44,23 +43,17 @@ class TfIdfTest(unittest.TestCase):
 
   def testAddCorpus(self):
     """Test adding input documents to the corpus."""
-    my_tfidf = tfidf.TfIdf("tfidf_testcorpus.txt", \
-                           DEFAULT_IDF = DEFAULT_IDF_UNITTEST)
+    my_tfidf = tfidf.TfIdf("tfidf_testcorpus.txt", DEFAULT_IDF = DEFAULT_IDF_UNITTEST)
 
     self.assertEquals(DEFAULT_IDF_UNITTEST, my_tfidf.get_idf("water"))
-    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 1),
-      my_tfidf.get_idf("moon"))
-    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 5),
-      my_tfidf.get_idf("said"))
+    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 1), my_tfidf.get_idf("moon"))
+    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 5), my_tfidf.get_idf("said"))
 
     my_tfidf.add_input_document("water, moon")
 
-    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 1),
-      my_tfidf.get_idf("water"))
-    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 2),
-      my_tfidf.get_idf("moon"))
-    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 5),
-      my_tfidf.get_idf("said"))
+    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 1), my_tfidf.get_idf("water"))
+    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 2), my_tfidf.get_idf("moon"))
+    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 5), my_tfidf.get_idf("said"))
 
   def testNoCorpusFiles(self):
     my_tfidf = tfidf.TfIdf(DEFAULT_IDF = DEFAULT_IDF_UNITTEST)
@@ -73,28 +66,22 @@ class TfIdfTest(unittest.TestCase):
     my_tfidf.add_input_document("moon said hello")
 
     self.assertEquals(DEFAULT_IDF_UNITTEST, my_tfidf.get_idf("water"))
-    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 1),
-      my_tfidf.get_idf("said"))
-    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 2),
-      my_tfidf.get_idf("moon"))
+    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 1), my_tfidf.get_idf("said"))
+    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 2), my_tfidf.get_idf("moon"))
 
   def testStopwordFile(self):
-    my_tfidf = tfidf.TfIdf("tfidf_testcorpus.txt", "tfidf_teststopwords.txt",
-                           DEFAULT_IDF = DEFAULT_IDF_UNITTEST)
+    my_tfidf = tfidf.TfIdf("tfidf_testcorpus.txt", "tfidf_teststopwords.txt", DEFAULT_IDF = DEFAULT_IDF_UNITTEST)
 
     self.assertEquals(DEFAULT_IDF_UNITTEST, my_tfidf.get_idf("water"))
     self.assertEquals(0, my_tfidf.get_idf("moon"))
-    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 5),
-      my_tfidf.get_idf("said"))
+    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 5), my_tfidf.get_idf("said"))
 
     my_tfidf.add_input_document("moon")
     my_tfidf.add_input_document("moon and water")
 
-    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 1),
-                            my_tfidf.get_idf("water"))
+    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 1), my_tfidf.get_idf("water"))
     self.assertEquals(0, my_tfidf.get_idf("moon"))
-    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 5),
-      my_tfidf.get_idf("said"))
+    self.assertAlmostEquals(get_expected_idf(my_tfidf.get_num_docs(), 5), my_tfidf.get_idf("said"))
 
 
 def main():
