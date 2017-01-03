b9fade01391900811a7f7e7a4b2a533782e487ca
apfannenstein <alpfannenstein@gmail.com>
2017-01-03 17:31:22 -0500

    attempt 2 at modularizing the existing TFIDF code -- supressed TFIDF calculations to reviewers (test) strictly; no review of businesses

--- tfidf/myTfidfTest.py
+++ tfidf/myTfidfTest.py
@@ -14,94 +14,99 @@ DEFAULT_IDF_UNITTEST = 1.0
 test_cond = '12_45'
 start = time.clock();
 
+
+def printTime() :
+    timeElapse = time.clock()-start 
+    print '\t%.2f seconds elapsed -- %.2f minutes'%(timeElapse, timeElapse/60)
+
+
+def initialize() :
+    print '\nBuilding index lookups...'
+    global reviewer_idx
+    global business_idx
+    for n in range(R) : reviewer_idx[data['Train Reviewer List'][n]] = n
+    for n in range(B) : business_idx[data['Reviewed Business List'][n]] = n
+    printTime()
+
+
+# Build sparse matrix 'A' with 'business X reviewer = review' info
+def buildIndex() :
+    print '\nBuilding business review idx'
+    reviewer = []
+    business = []
+    review = []
+    for uid in reviewer_idx :
+        for rid in data['Reviewer Reviews'][uid] :
+            reviewInfo = data['Review Information'][rid]
+            bid = reviewInfo['business_id']
+            # make unique ID for accessing text reviews between bid x rid
+            reviewCounter = int(''.join([str(business_idx[bid]),str(reviewer_idx[uid])]))
+            reviewText = reviewInfo['text']
+            if len(reviewText) > 10 :
+                review_idx[reviewCounter] = reviewText
+            # Values for the CSR Matrix
+            reviewer.append(reviewer_idx[uid]) # r
+            business.append(business_idx[bid]) # c
+            review.append(reviewCounter)       # d
+    A = sp.csr_matrix((review, (business, reviewer)),shape=[B,R])
+    A.sort_indices()
+    printTime()
+    return A
+
+
+# For each review written to business in test_lst
+#   Push each review's text into a compiled "document"
+#   then add "doc" as input_str to appropriate TF-IDF instance
+def reviewTFIDF(rTFIDF, lst) :
+    rDoc = ''
+    for review in lst:
+        if review in review_idx :
+            rDoc += review_idx[review]
+    rTFIDF.add_input_str(rDoc)
+    return rTFIDF
+
+
+# Obtain keywords for reviewer TFIDF and publish to .score files
+def writeScores() :
+    print 'Running evaluation...';
+    score_dir = 'scores/TFIDF_%s'%(test_cond);
+    os.system('mkdir -p %s'%(score_dir));
+    os.system('rm %s/*'%(score_dir));
+    here = os.path.dirname(os.path.realpath(__file__));
+    for reviewer in test_reviewer_lst :
+        [train_lst,test_lst] = util.read_key('lists_%s/%s.key'%(test_cond,reviewer),business_idx);
+        outfile = '%s/%s.scores'%(score_dir,reviewer);
+        fid = open(outfile,'w');
+        print "\nOPENING file %s"%(outfile)
+    
+        reviewer_tfidf = tfidf.TfIdf("tfidf_teststopwords.txt")	# TFIDF instance for each reviewer
+        for (b,i,l) in test_lst :
+            # Get the list of reviews given to business in test_lst
+            train_reviews = A.getrow(business_idx[b]).tocoo().data
+            reviewer_tfidf = reviewTFIDF(reviewer_tfidf, train_reviews)
+    
+        # Summary of information for the reviewer from all TEST review
+        tokens = reviewer_tfidf.return_tokens()
+        keywords = reviewer_tfidf.get_doc_keywords()
+        fid.write("\n\nReviewer" + str(reviewer) + "\twith test reviews of " + str(len(test_lst)) + "\n")
+        for word in keywords[:10] : 
+            fid.write("\n\tWORD: %s\tTF:%.0f\tIDF:%.3f\tTF-IDF:%.3f" %(str(word[0]),tokens[word[0]],reviewer_tfidf.get_idf(word[0]),word[1]))
+        fid.write("\n")
+        fid.close()
+    printTime()
+
+
+reviewer_idx = {}
+business_idx = {}
+review_idx = {} 
 execfile('../load_edge_attr_data.py');
 B = len(data['Reviewed Business List'])
 R = len(data['Train Reviewer List'])
 D = len(data['Reviewer Reviews'])
-print '   %.2f seconds elapsed'%(time.clock()-start);
 
-print '\nBuilding index lookups...'
-reviewer_idx = {}
-business_idx = {}
-review_idx = {} 
-for n in range(R) : reviewer_idx[data['Train Reviewer List'][n]] = n
-for n in range(B) : business_idx[data['Reviewed Business List'][n]] = n
-print '   %.2f seconds elapsed'%(time.clock()-start);
-
-print '\nBuilding business review idx'
-reviewer = []
-business = []
-review = []
-for uid in reviewer_idx :
-    for rid in data['Reviewer Reviews'][uid] :
-        reviewInfo = data['Review Information'][rid]
-        bid = reviewInfo['business_id']
-
-        # make unique ID for accessing text reviews between bid x rid
-        reviewCounter = int(''.join([str(business_idx[bid]),str(reviewer_idx[uid])]))
-        if reviewCounter == 4255788 :
-            print reviewInfo['text']
-        reviewtext = reviewInfo['text']
-        if len(reviewtext) > 10 :
-            review_idx[reviewCounter] = reviewInfo['text']
-        # Values for the CSR Matrix
-        reviewer.append(reviewer_idx[uid]) # r
-        business.append(business_idx[bid]) # c
-        review.append(reviewCounter)      # d
-A = sp.csr_matrix((review, (business, reviewer)),shape=[B,R])
-A.sort_indices()
-print '    %.2f seconds elapsed'%(time.clock()-start);
+initialize()
+A = buildIndex()
+writeScores()
 
 
-## Enter data to .scores files
-#############################
-print 'Running evaluation...';
-score_dir = 'scores/TFIDF_%s'%(test_cond);
-os.system('mkdir -p %s'%(score_dir));
-os.system('rm %s/*'%(score_dir));
-here = os.path.dirname(os.path.realpath(__file__));
-reviewCount = 0
-for reviewer in test_reviewer_lst :
-    [train_lst,test_lst] = util.read_key('lists_%s/%s.key'%(test_cond,reviewer),business_idx);
-    outfile = '%s/%s.scores'%(score_dir,reviewer);
-    fid = open(outfile,'w');
-    reviewCount += 1
-    print "\nOPENING file %s"%(outfile)
-
-    reviewer_tfidf = tfidf.TfIdf("tfidf_teststopwords.txt")	# TFIDF instance for each reviewer
-    for (b,i,l) in test_lst :
-        # Get the list of reviews given to business in test_lst
-        train_reviews = A.getrow(business_idx[b]).tocoo().data
-        print '\tFor business ' + str(b) + '\t' + 'Train businesses for test: ' + str(len(train_reviews))
-        fid.write('\nFor business ' + str(b) + '\t' + 'Train businesses for test: ' + str(len(train_reviews)))
-
-        train_tfidf = tfidf.TfIdf("tfidf_teststopwords.txt")	# TFIDF instance for each business
-        reviewer_doc = ''
-        # For each review written to business in test_lst
-        #   Push each review's text into a compiled "document"
-        #   then add "doc" as input_str to appropriate TF-IDF instance
-        for review in train_reviews :
-            train_doc = ''
-            if review in review_idx :
-                train_doc += review_idx[review]
-                reviewer_doc += review_idx[review]
-            train_tfidf.add_input_str(train_doc)
-        reviewer_tfidf.add_input_str(reviewer_doc)
-
-        # Summary of information for each test businesses' TFIDF
-        tokens = train_tfidf.return_tokens()
-        keywords = train_tfidf.get_doc_keywords()
-        for word in keywords[:5] : 
-            fid.write("\n\tWORD: %s\tTF:%.0f\tIDF:%.3f\tTF-IDF:%.3f" %(str(word[0]),tokens[word[0]],train_tfidf.get_idf(word[0]),word[1]))
-
-    # Summary of information for the reviewer from all TEST review
-    tokens = reviewer_tfidf.return_tokens()
-    keywords = reviewer_tfidf.get_doc_keywords()
-    fid.write("\n\nReviewer" + str(reviewer) + "\twith test reviews of " + str(len(test_lst)) + "\n")
-    for word in keywords[:10] : 
-        fid.write("\n\tWORD: %s\tTF:%.0f\tIDF:%.3f\tTF-IDF:%.3f" %(str(word[0]),tokens[word[0]],reviewer_tfidf.get_idf(word[0]),word[1]))
-    fid.write("\n")
-    fid.close()
-
-    print 'Reviewer %d out of %d --- \t%.2f seconds elapsed'%(reviewCount, len(test_reviewer_lst), time.clock()-start);
-print '%.2f seconds elapsed'%(time.clock()-start);
+# EOF
