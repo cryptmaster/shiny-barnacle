2f07a6d4fb9d5e1d1cea12c16046401a6c03d903
apfannenstein <alpfannenstein@gmail.com>
2017-01-18 14:25:39 -0500

    added new section based on the bag-of-words example from book

--- tfidf/sklearnTfidf.py
+++ tfidf/sklearnTfidf.py
@@ -10,7 +10,11 @@ import operator
 import tfidf
 import sklearn.feature_extraction.text as text
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import NMF
+from sklearn.pipeline import make_pipeline
+from sklearn import linear_model, neighbors
 import sklearn.metrics as metrics
 
 sys.path.append('/home/hltcoe/gsell/tools/python_mods/');
@@ -91,6 +95,106 @@ def initialBuildIndex() :
     return A
 
 
+# Bag-of-word example from Introduction to Machine Learning
+# ..with Python by O'Reily
+def bagOfWords(X_train, y_train, X_test, y_test) :
+#    vect = CountVectorizer().fit(X_train)
+#    X_train = vect.transform(X_train)
+#    print(repr(X_train))
+
+    no_features = 100
+    vect = TfidfVectorizer(ngram_range=(1,3), max_features=no_features, stop_words='english')
+    X_train = vect.fit_transform(X_train)
+    print(repr(X_train))
+
+    feature_names = vect.get_feature_names()
+    print(len(feature_names))
+    # print first 20 features
+    print(feature_names[:20])
+    # get every 20th word to get an overview
+    print(feature_names[::20])
+
+    scores = cross_val_score(LogisticRegression(), X_train, y_train, cv=5)
+    np.mean(scores)
+
+    param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100]}
+    grid = GridSearchCV(LogisticRegression(), param_grid, cv=5)
+    grid.fit(X_train, y_train)
+    print ("Best cross-validation score: ", grid.best_score_)
+    print ("Best parameters: ", grid.best_params_)
+
+    X_test = vect.transform(X_test)
+    grid.score(X_test, y_test)
+
+
+# use Train data and Test data for each reviewer
+def trainTest() :
+    for reviewer in test_reviewer_lst :
+        [train_lst,test_lst] = util.read_key('lists_%s/%s.key'%(test_cond,reviewer),business_idx);
+        # Build positive and negative dictionary based on 
+        # ..reviewer's past data
+        print '\n\nPROCESSING FOR REVIEWER %s'%(str(reviewer))
+        posDic = []
+        negDic = []
+        pos_reviews = []
+        neg_reviews = []
+        X_train = []
+        y_train = []
+        X_test = []
+        y_test = []
+        for (b,i,l) in train_lst :
+            if l == 1 :
+                pos_reviews = buildReviewLst(A[1].getrow(business_idx[b]).tocoo().data)
+                #posDic += buildDictionary(pos_reviews)
+                X_train.append(buildReviewLst(pos_reviews))
+            else :
+                neg_reviews = buildReviewLst(A[-1].getrow(business_idx[b]).tocoo().data)
+                #negDic += buildDictionary(neg_reviews)
+                X_train.append(buildReviewLst(neg_reviews))
+            y_train.append(l)
+
+        # Ensure dictionaries are free of duplicates
+#        print '----------- Positive Dictionary: Topic Extraction using NMF -----------' 
+#        posDic = remove_duplicates(posDic)
+#        print '----------- Negative Dictionary: Topic Extraction using NMF -----------' 
+#        negDic = remove_duplicates(negDic)
+#        for word in negDic[:] :
+#            if word in posDic :
+#                posDic.remove(word)
+#                negDic.remove(word)
+
+        # Determine 'confidence' of predictive label based on presence
+        # ...of business review's words in posDic && negDic
+        confidence = 0
+        for (b,i,l) in test_lst :
+            for s in [-1,1] :
+                train_reviews = A[s].getrow(business_idx[b]).tocoo().data
+            reviews = buildReviewLst(train_reviews)
+            X_test.append(reviews)
+            y_test.append(l)
+#            userRating = float(buildVector(reviews, posDic, negDic))*100
+#            if userRating > .75 :
+#                label = 1
+#            else :
+#                label = -1
+
+#            if str(label) == str(l) :
+#                confidence += 1
+
+        # Provide summary of intel
+#        accuracy = (float(confidence)/len(test_lst))*100
+#        print "\n\nOVERALL STATS FOR REVIEWER: " + str(reviewer)
+#        print "\tPositive Dictionary: \t"+ str(len(posDic))
+#        print "\tNegative Dictionary: \t"+ str(len(negDic))
+#        print "\n\tBusinesses trained: \t" + str(len(train_lst))
+#        print "\tBusinesses tested: \t" + str(len(test_lst))
+#        print "\n\tAccurate predictions:\t%d"%(confidence)
+#        print "\tAccuracy Rating: \t%.2f%%"%(accuracy) 
+#        print ()
+        bagOfWords(X_train, y_train, X_test, y_test)
+        printTime()
+
+
 # Magic
 def buildTFIDF(corpus, topicModel) :
     no_features = 100
@@ -98,42 +202,24 @@ def buildTFIDF(corpus, topicModel) :
     tfidf = tfidf_vectorizer.fit_transform(corpus)
     tfidf_feature_names = tfidf_vectorizer.get_feature_names()
     idf = tfidf_vectorizer.idf_
-
     vectorDict = dict(zip(tfidf_feature_names, idf))
     sortedDict = sorted(vectorDict.items(), key=operator.itemgetter(1), reverse=True)
-
     if topicModel :
         display_topics(tfidf, tfidf_feature_names)
 
     return sortedDict
+
 
 # Prints no_top_words for each feature
 def display_topics(model, feature_names) :
     no_topics = 5
     no_top_words = 10
     nmf = NMF(n_components=no_topics, random_state=1).fit(model)
-
     for topic_idx, topic in enumerate(nmf.components_):
         print "Topic #%d:" %(topic_idx)
-        print " ".join([feature_names[i]
+        print " ,".join([feature_names[i]
                 for i in topic.argsort()[:-no_top_words - 1:-1]])
         print ''
-
-
-# Creates TFIDF weighted list of verbiage
-def buildVector(corpus, posDic, negDic) :
-    userValue = 0
-    if len(corpus) > 0 :
-        sortedDict = buildTFIDF(corpus, False)
-
-        userRating = 0
-        totalRating = 0
-        for (word,value) in sortedDict :
-            totalRating += value
-            if word in posDic : userRating += value
-            if word in negDic : userRating -= value 
-        if userRating > 0 : userValue = float(userRating)/totalRating
-    return userValue
 
 
 # Create a dictionary of words from review
@@ -141,7 +227,7 @@ def buildDictionary(review) :
     dictionary = []
     if len(review) > 0 :
         sortedDict = buildTFIDF(review, False)
-        for word in sortedDict[:10] : dictionary.append(word[0])
+        for word in sortedDict : dictionary.append(word[0])
     return dictionary 
 
 
@@ -165,64 +251,23 @@ def remove_duplicates(values) :
             output.append(value)
             seen.add(value)
     output = buildTFIDF(output, True)
-
     return output
 
 
-# use Train data and Test data for each reviewer
-def trainTest() :
-    for reviewer in test_reviewer_lst :
-        [train_lst,test_lst] = util.read_key('lists_%s/%s.key'%(test_cond,reviewer),business_idx);
-        # Build positive and negative dictionary based on 
-        # ..reviewer's past data
-        posDic = []
-        negDic = []
-        pos_reviews = []
-        neg_reviews = []
-        for (b,i,l) in train_lst :
-            if l == 1 :
-                pos_reviews = buildReviewLst(A[1].getrow(business_idx[b]).tocoo().data)
-                print '----------------- Positive ----------------- '
-                posDic += buildDictionary(pos_reviews)
-            else :
-                neg_reviews = buildReviewLst(A[-1].getrow(business_idx[b]).tocoo().data)
-                print '----------------- Negative ----------------- '
-                negDic += buildDictionary(neg_reviews)
+# Creates TFIDF weighted list of verbiage
+def buildVector(corpus, posDic, negDic) :
+    userValue = 0
+    if len(corpus) > 0 :
+        sortedDict = buildTFIDF(corpus, False)
 
-        # Ensure dictionaries are free of duplicates
-        posDic = remove_duplicates(posDic)
-        negDic = remove_duplicates(negDic)
-        for word in negDic[:] :
-            if word in posDic :
-                posDic.remove(word)
-                negDic.remove(word)
-
-        # Determine 'confidence' of predictive label based on presence
-        # ...of business review's words in posDic && negDic
-        confidence = 0
-        for (b,i,l) in test_lst :
-            for s in [-1,1] :
-                train_reviews = A[s].getrow(business_idx[b]).tocoo().data
-            reviews = buildReviewLst(train_reviews)
-            userRating = float(buildVector(reviews, posDic, negDic))*100
-            if userRating > .75 :
-                label = 1
-            else :
-                label = -1
-
-            if str(label) == str(l) :
-                confidence += 1
-
-        # Provide summary of intel
-        accuracy = (float(confidence)/len(test_lst))*100
-        print "\n\nOVERALL STATS FOR REVIEWER: " + str(reviewer)
-        print "\tPositive Dictionary: \t"+ str(len(posDic))
-        print "\tNegative Dictionary: \t"+ str(len(negDic))
-        print "\n\tBusinesses trained: \t" + str(len(train_lst))
-        print "\tBusinesses tested: \t" + str(len(test_lst))
-        print "\n\tAccurate predictions:\t%d"%(confidence)
-        print "\tAccuracy Rating: \t%.2f%%"%(accuracy) 
-        printTime()
+        userRating = 0
+        totalRating = 0
+        for (word,value) in sortedDict :
+            totalRating += value
+            if word in posDic : userRating += value
+            if word in negDic : userRating -= value 
+        if userRating > 0 : userValue = float(userRating)/totalRating
+    return userValue
 
 
 
